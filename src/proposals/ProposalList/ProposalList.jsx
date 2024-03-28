259ad1da0f9b44edac3a774aa1261e60494fde94
Candidate <>
2024-03-28 09:46:39 +0000

    Git sync

--- src/proposals/ProposalList/ProposalList.jsx
+++ src/proposals/ProposalList/ProposalList.jsx
@@ -2,10 +2,14 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import ProposalRow from "../ProposalRow";
+import { getTalks } from "../../api/httpApi";
 
 import "./ProposalList.css";
 
 const ProposalList = () => {
+    useEffect(() => {
+const data = 
+    }, [])
    
     return (
         <ul data-testid="proposal-list" className="ProposalList">
