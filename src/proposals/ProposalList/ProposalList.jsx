667466048b88a29fd6d5273e58825e0f90788537
Candidate <>
2024-03-28 09:41:09 +0000

    Git sync

--- src/proposals/ProposalList/ProposalList.jsx
+++ src/proposals/ProposalList/ProposalList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import ProposalRow from "../ProposalRow";
-inport ProposalList
+import 
 
 import "./ProposalList.css";
 
