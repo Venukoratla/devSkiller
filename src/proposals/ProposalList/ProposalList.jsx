b3d37c98636c7838b2166c14c8ebd0522577c244
Candidate <>
2024-03-28 09:37:09 +0000

    Git sync

--- src/proposals/ProposalList/ProposalList.jsx
+++ src/proposals/ProposalList/ProposalList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import ProposalRow from "../ProposalRow";
@@ -9,30 +9,35 @@ import "./ProposalList.css";
 const ProposalList = () => {
 
     const getData = async () => {
-const data = await setupFakeHTTP()
+        const data = await setupFakeHTTP()
+        console.log(data)
     }
 
-    
+    useEffect(() => {
+        getData
+    })
+
     return (
-    <ul data-testid="proposal-list" className="ProposalList">
-        {[].map((proposal) => (
-            <li
-                key={proposal.id}
-                className="ProposalList__item"
-            >
-                <Link
+        <ul data-testid="proposal-list" className="ProposalList">
+            {[].map((proposal) => (
+                <li
                     key={proposal.id}
-                    className="ProposalList__item__link"
-                    to={`/proposals`}
+                    className="ProposalList__item"
                 >
-                    <ProposalRow
-                        proposal={proposal}
-                        onStatusUpdate={() => {}}
-                    />
-                </Link>
-            </li>
-        ))}
-    </ul>
-)};
+                    <Link
+                        key={proposal.id}
+                        className="ProposalList__item__link"
+                        to={`/proposals`}
+                    >
+                        <ProposalRow
+                            proposal={proposal}
+                            onStatusUpdate={() => { }}
+                        />
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+};
 
 export default ProposalList;
